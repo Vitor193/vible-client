d3f2567ed12183c4e4b309f236ed39804fb5e9d5
Vitor193 <vitormiguel135@gmail.com>
2024-03-25 16:30:17 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import {Routes,Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
 import './App.css'
 
 function App() {
  
   return (
     <div className='App'>
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element= {<Home />} />
+        
+      </Routes>
     </div>
   )
 }
